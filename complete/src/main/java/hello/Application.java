b213803c405f8d0a039839a23db2d93b8ad22b75
Greg Turnquist <gturnquist@gopivotal.com>
2013-07-11 10:03:34 -0500

    Initial code commit for this guide

---
+++ complete/src/main/java/hello/Application.java
@@ -0,0 +1,73 @@
+package hello;
+
+import javax.sql.DataSource;
+
+import org.junit.Assert;
+import org.springframework.bootstrap.SpringApplication;
+import org.springframework.bootstrap.context.annotation.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableAutoConfiguration
+public class Application {
+	
+	@Bean
+	BookingService bookingService() {
+		return new BookingService();
+	}
+
+	@Bean
+	DataSource dataSource() {
+		return new SimpleDriverDataSource() {{
+	        setDriverClass(org.h2.Driver.class);
+	        setUsername("sa");
+	        setUrl("jdbc:h2:mem");
+	        setPassword("");
+		}};
+	}
+	
+	@Bean
+	JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        System.out.println("Creating tables");
+        jdbcTemplate.execute("drop table BOOKINGS if exists");
+        jdbcTemplate.execute("create table BOOKINGS(" +
+                "ID serial, FIRST_NAME varchar(5) NOT NULL)");
+		return jdbcTemplate;
+	}
+	
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		
+		BookingService bookingService = ctx.getBean(BookingService.class);
+		bookingService.book("Greg", "Roy", "Craig");
+		Assert.assertEquals("First booking should work with no problem",
+				3, bookingService.findAllBookings().size());
+		
+		try {
+			bookingService.book("Chris", "Samuel");
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		Assert.assertEquals("'Samual' should have triggered a rollback",
+				3, bookingService.findAllBookings().size());
+
+		try {
+			bookingService.book("Buddy", null);
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		Assert.assertEquals("'null' should have triggered a rollback",
+				3, bookingService.findAllBookings().size());
+
+	}
+
+}
---
+++ complete/src/main/java/hello/BookingService.java
@@ -0,0 +1,34 @@
+package hello;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Transactional;
+
+public class BookingService {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	@Transactional
+	public void book(String... persons) {
+		for (String person : persons) {
+			System.out.println("Booking " + person + " in a seat...");
+			jdbcTemplate.update("insert into BOOKINGS(FIRST_NAME) values (?)", person);
+		}
+	};
+
+	public List<String> findAllBookings() {
+		return jdbcTemplate.query("select FIRST_NAME from BOOKINGS", new RowMapper<String>() {
+			@Override
+			public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return rs.getString("FIRST_NAME");
+			}
+		});
+	}
+
+}
