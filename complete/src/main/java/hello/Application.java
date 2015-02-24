43c7398bf310b6b1f4b09f2239fc5c9f07bf09af
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-24 13:28:15 -0600

    Upgrade to @SpringBootApplicatiton and remove old cruft

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -5,18 +5,14 @@ import javax.sql.DataSource;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
-@EnableTransactionManagement
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
@@ -24,18 +20,6 @@ public class Application {
 	@Bean
 	BookingService bookingService() {
 		return new BookingService();
-	}
-
-	@Bean
-	DataSource dataSource() {
-		return new SimpleDriverDataSource() {
-			{
-				setDriverClass(org.h2.Driver.class);
-				setUsername("sa");
-				setUrl("jdbc:h2:mem");
-				setPassword("");
-			}
-		};
 	}
 
 	@Bean
