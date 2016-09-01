82fb20b232b671390dc55bf3e4d87b508695770a
Stephane Nicoll <snicoll@pivotal.io>
2016-09-01 10:08:45 +0200

    Polish

--- complete/src/main/java/hello/AppRunner.java
+++ complete/src/main/java/hello/AppRunner.java
@@ -3,10 +3,10 @@ package hello;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
-@Service
+@Component
 class AppRunner implements CommandLineRunner {
 
     private final static Logger logger = LoggerFactory.getLogger(AppRunner.class);
