c8f8aa5921295f2a590dbae6f06d43eb9d5393f6
Stephane Nicoll <snicoll@pivotal.io>
2016-09-01 09:55:45 +0200

    Polish
    This commit polishes the code and use more feature of Spring Boot. The
    schema is automatically created using `schema.sql` and the sample is
    executed via a CommandLineRunner.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,75 +1,13 @@
 package hello;
 
-import javax.sql.DataSource;
-
-import org.junit.Assert;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class Application {
 
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
-	@Bean
-	BookingService bookingService() {
-		return new BookingService();
-	}
-
-	@Bean
-	JdbcTemplate jdbcTemplate(DataSource dataSource) {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-		log.info("Creating tables");
-		jdbcTemplate.execute("drop table BOOKINGS if exists");
-		jdbcTemplate.execute("create table BOOKINGS("
-				+ "ID serial, FIRST_NAME varchar(5) NOT NULL)");
-		return jdbcTemplate;
-	}
-
-	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-		BookingService bookingService = ctx.getBean(BookingService.class);
-		bookingService.book("Alice", "Bob", "Carol");
-		Assert.assertEquals("First booking should work with no problem", 3,
-				bookingService.findAllBookings().size());
-
-		try {
-			bookingService.book("Chris", "Samuel");
-		}
-		catch (RuntimeException e) {
-			log.info("v--- The following exception is expect because 'Samuel' is too big for the DB ---v");
-			log.error(e.getMessage());
-		}
-
-		for (String person : bookingService.findAllBookings()) {
-			log.info("So far, " + person + " is booked.");
-		}
-		log.info("You shouldn't see Chris or Samuel. Samuel violated DB constraints, and Chris was rolled back in the same TX");
-		Assert.assertEquals("'Samuel' should have triggered a rollback", 3,
-				bookingService.findAllBookings().size());
-
-		try {
-			bookingService.book("Buddy", null);
-		}
-		catch (RuntimeException e) {
-			log.info("v--- The following exception is expect because null is not valid for the DB ---v");
-			log.error(e.getMessage());
-		}
-
-		for (String person : bookingService.findAllBookings()) {
-			log.info("So far, " + person + " is booked.");
-		}
-		log.info("You shouldn't see Buddy or null. null violated DB constraints, and Buddy was rolled back in the same TX");
-		Assert.assertEquals("'null' should have triggered a rollback", 3, bookingService
-				.findAllBookings().size());
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
--- complete/src/main/java/hello/BookingService.java
+++ complete/src/main/java/hello/BookingService.java
@@ -1,38 +1,35 @@
 package hello;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Component
 public class BookingService {
 
-	private final static Logger log = LoggerFactory.getLogger(BookingService.class);
-    
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-    
+    private final static Logger logger = LoggerFactory.getLogger(BookingService.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public BookingService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Transactional
     public void book(String... persons) {
         for (String person : persons) {
-            log.info("Booking " + person + " in a seat...");
+            logger.info("Booking " + person + " in a seat...");
             jdbcTemplate.update("insert into BOOKINGS(FIRST_NAME) values (?)", person);
         }
-    };
+    }
 
     public List<String> findAllBookings() {
-        return jdbcTemplate.query("select FIRST_NAME from BOOKINGS", new RowMapper<String>() {
-            @Override
-            public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-                return rs.getString("FIRST_NAME");
-            }
-        });
+        return jdbcTemplate.query("select FIRST_NAME from BOOKINGS",
+                (rs, rowNum) -> rs.getString("FIRST_NAME"));
     }
 
 }
