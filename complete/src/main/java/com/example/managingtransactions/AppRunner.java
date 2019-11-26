820ec27f548b25702aa9a1d8724fc72765792e55
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:08:28 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/com/example/managingtransactions/AppRunner.java
+++ complete/src/main/java/com/example/managingtransactions/AppRunner.java
@@ -9,51 +9,51 @@ import org.springframework.util.Assert;
 @Component
 class AppRunner implements CommandLineRunner {
 
-    private final static Logger logger = LoggerFactory.getLogger(AppRunner.class);
+	private final static Logger logger = LoggerFactory.getLogger(AppRunner.class);
 
-    private final BookingService bookingService;
+	private final BookingService bookingService;
 
-    public AppRunner(BookingService bookingService) {
-        this.bookingService = bookingService;
-    }
+	public AppRunner(BookingService bookingService) {
+		this.bookingService = bookingService;
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        bookingService.book("Alice", "Bob", "Carol");
-        Assert.isTrue(bookingService.findAllBookings().size() == 3,
-                "First booking should work with no problem");
-        logger.info("Alice, Bob and Carol have been booked");
-        try {
-            bookingService.book("Chris", "Samuel");
-        } catch (RuntimeException e) {
-            logger.info("v--- The following exception is expect because 'Samuel' is too " +
-                    "big for the DB ---v");
-            logger.error(e.getMessage());
-        }
+	@Override
+	public void run(String... args) throws Exception {
+		bookingService.book("Alice", "Bob", "Carol");
+		Assert.isTrue(bookingService.findAllBookings().size() == 3,
+				"First booking should work with no problem");
+		logger.info("Alice, Bob and Carol have been booked");
+		try {
+			bookingService.book("Chris", "Samuel");
+		} catch (RuntimeException e) {
+			logger.info("v--- The following exception is expect because 'Samuel' is too " +
+					"big for the DB ---v");
+			logger.error(e.getMessage());
+		}
 
-        for (String person : bookingService.findAllBookings()) {
-            logger.info("So far, " + person + " is booked.");
-        }
-        logger.info("You shouldn't see Chris or Samuel. Samuel violated DB constraints, " +
-                "and Chris was rolled back in the same TX");
-        Assert.isTrue(bookingService.findAllBookings().size() == 3,
-                "'Samuel' should have triggered a rollback");
+		for (String person : bookingService.findAllBookings()) {
+			logger.info("So far, " + person + " is booked.");
+		}
+		logger.info("You shouldn't see Chris or Samuel. Samuel violated DB constraints, " +
+				"and Chris was rolled back in the same TX");
+		Assert.isTrue(bookingService.findAllBookings().size() == 3,
+				"'Samuel' should have triggered a rollback");
 
-        try {
-            bookingService.book("Buddy", null);
-        } catch (RuntimeException e) {
-            logger.info("v--- The following exception is expect because null is not " +
-                    "valid for the DB ---v");
-            logger.error(e.getMessage());
-        }
+		try {
+			bookingService.book("Buddy", null);
+		} catch (RuntimeException e) {
+			logger.info("v--- The following exception is expect because null is not " +
+					"valid for the DB ---v");
+			logger.error(e.getMessage());
+		}
 
-        for (String person : bookingService.findAllBookings()) {
-            logger.info("So far, " + person + " is booked.");
-        }
-        logger.info("You shouldn't see Buddy or null. null violated DB constraints, and " +
-                "Buddy was rolled back in the same TX");
-        Assert.isTrue(bookingService.findAllBookings().size() == 3,
-                "'null' should have triggered a rollback");
-    }
+		for (String person : bookingService.findAllBookings()) {
+			logger.info("So far, " + person + " is booked.");
+		}
+		logger.info("You shouldn't see Buddy or null. null violated DB constraints, and " +
+				"Buddy was rolled back in the same TX");
+		Assert.isTrue(bookingService.findAllBookings().size() == 3,
+				"'null' should have triggered a rollback");
+	}
 
 }
--- complete/src/main/java/com/example/managingtransactions/BookingService.java
+++ complete/src/main/java/com/example/managingtransactions/BookingService.java
@@ -11,25 +11,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 public class BookingService {
 
-    private final static Logger logger = LoggerFactory.getLogger(BookingService.class);
+	private final static Logger logger = LoggerFactory.getLogger(BookingService.class);
 
-    private final JdbcTemplate jdbcTemplate;
+	private final JdbcTemplate jdbcTemplate;
 
-    public BookingService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+	public BookingService(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
-    @Transactional
-    public void book(String... persons) {
-        for (String person : persons) {
-            logger.info("Booking " + person + " in a seat...");
-            jdbcTemplate.update("insert into BOOKINGS(FIRST_NAME) values (?)", person);
-        }
-    }
+	@Transactional
+	public void book(String... persons) {
+		for (String person : persons) {
+			logger.info("Booking " + person + " in a seat...");
+			jdbcTemplate.update("insert into BOOKINGS(FIRST_NAME) values (?)", person);
+		}
+	}
 
-    public List<String> findAllBookings() {
-        return jdbcTemplate.query("select FIRST_NAME from BOOKINGS",
-                (rs, rowNum) -> rs.getString("FIRST_NAME"));
-    }
+	public List<String> findAllBookings() {
+		return jdbcTemplate.query("select FIRST_NAME from BOOKINGS",
+				(rs, rowNum) -> rs.getString("FIRST_NAME"));
+	}
 
 }
